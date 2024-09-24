5c8fcd8699a6313d70474860113f82f0500fd881
Gilmar Alcantara <gilmar.alcantara@hotmart.com>
2024-09-23 23:13:06 -0300

    Add task_definitions

--- variables.tf
+++ variables.tf
@@ -2,6 +2,8 @@ variable "service_name" {}
 
 variable "cluster_name" {}
 
+variable "region" {}
+
 variable "vpc_id" {}
 
 variable "private_subnets" {}
@@ -15,3 +17,11 @@ variable "service_memory" {}
 variable "service_listener" {}
 
 variable "service_task_execution_role" {}
+
+variable "environment_variables" {
+  type = list(any)
+}
+
+variable "capabilities" {
+  type = list(any)
+}
